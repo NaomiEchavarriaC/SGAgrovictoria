55b6b32a812f1328ff5386037efae2e3815ee7cf
Andrés Canul Bolaños <98436130+AndresCanul@users.noreply.github.com>
2024-08-22 12:01:57 -0600

    Actualizacion

--- SGAgrovictoriaWEB/SGAgrovictoriaWEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SGAgrovictoriaWEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
