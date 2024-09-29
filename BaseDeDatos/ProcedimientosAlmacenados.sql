a465ffbf38019c8cd5159de10c77051c06105a52
Andrés Canul Bolaños <98436130+AndresCanul@users.noreply.github.com>
2024-09-29 15:24:55 -0600

    InsercionDatos

--- BaseDeDatos/ProcedimientosAlmacenados.sql
+++ BaseDeDatos/ProcedimientosAlmacenados.sql
@@ -1,5 +1,37 @@
 
 -------------------------------------------------------------------------
+-- PROCEDIMIENTOS ALMACENADOS DE DISTRITOS
+-------------------------------------------------------------------------
+
+USE [SGAgroVictoriaDB]
+GO
+
+CREATE PROCEDURE SP_GET_UBICACION
+AS
+BEGIN
+    SELECT 		
+		D.NOMBRE_DISTRITO,
+		D.ID_CANTON,
+		C.NOMBRE_CANTON,
+		C.ID_CANTON,
+		P.NOMBRE_PROVINCIA,
+		P.ID_PROVINCIA
+    FROM 
+        DISTRITOS D
+    INNER JOIN 
+        CANTONES C
+			ON C.ID_CANTON = D.ID_CANTON
+    INNER JOIN 
+        PROVINCIAS P
+			ON C.ID_PROVINCIA = P.ID_PROVINCIA
+    ORDER BY 
+        P.ID_PROVINCIA, 
+        C.ID_CANTON, 
+        D.ID_DISTRITO;
+END;
+GO
+
+-------------------------------------------------------------------------
 -- PROCEDIMIENTOS ALMACENADOS DE CREDENCIALES
 -------------------------------------------------------------------------
 
