44f4ffdb90467c479958e1f37dc262a485268575
DaniLopAr <dj01lopeza@gmail.com>
2024-10-22 16:20:28 -0600

    Alertas controller y model
    No funcional pero para trabajarlo independientemente con ese controller y model

---
+++ SGAgrovictoriaWEB/SGAgrovictoriaWEB/Controllers/AlertaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SGAgrovictoriaWEB.Data;
+using SGAgrovictoriaWEB.Models;
+namespace SGAgrovictoriaWEB.Controllers
+{
+    public class AlertaController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<AlertaController> _logger;
+
+        public AlertaController(ILogger<AlertaController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> AlertaStockBajo()
+        {
+            // Obtener todos los productos de la base de datos
+            var ListaProductos = await _context.Productos.ToListAsync();
+
+            // Definir el umbral de stock bajo
+            int umbralStockBajo = 10;
+
+            // Filtrar los productos que tengan un stock bajo
+            var productosConStockBajo = ListaProductos.Where(p => p.Stock <= umbralStockBajo).ToList();
+
+            // Pasar ambos, la lista completa de productos y los productos con stock bajo a la vista
+            ViewBag.ProductosConStockBajo = productosConStockBajo;
+
+            return View(ListaProductos);
+        }
+
+
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SGAgrovictoriaWEB/SGAgrovictoriaWEB/Models/AlertaModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SGAgrovictoriaWEB.Models
+{
+    public class AlertaModel
+    {
+        [Key]
+
+        public int IdAlerta { get; set; }
+
+        public string Descripcion { get; set; }
+        public long IdProducto { get; set; }
+
+        public string Categoria { get; set; }
+
+        public bool Estado { get; set; }
+
+        public string NombreProducto { get; set; }
+
+        //public int Stock { get; set; }
+        
+    }
+}
